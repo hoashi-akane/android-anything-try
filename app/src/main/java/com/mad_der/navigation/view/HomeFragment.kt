c31777a6d4d880d20c009f78aa11ad5fb5ba1bbd
HOASHI AKANE <akane.hoashi@outlook.jp>
2022-05-22 01:07:32 +0900

    ホーム画面にComposeのレイアウト追加

--- app/src/main/java/com/mad_der/navigation/view/HomeFragment.kt
+++ app/src/main/java/com/mad_der/navigation/view/HomeFragment.kt
@@ -4,17 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.Fragment
-import com.mad_der.navigation.R
+import com.mad_der.navigation.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        binding.composeView.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            this.setContent {
+                MakeMessage()
+            }
+        }
+        return binding.root
     }
+}
+
+@Composable
+fun MakeMessage() {
+    Text(text = "composer test", textAlign = TextAlign.Center)
+}
+
+@Preview
+@Composable
+fun PreviewView() {
+    MakeMessage()
 }
